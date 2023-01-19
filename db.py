06f5d3a908307ea19e711034d295041cc923c190
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2023-01-19 09:46:29 -0500

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://uzqembaougkjbd:fcc4f8eb2fcb83d6d39621c12f0a836c8f73284ddfee4494bd88b40a15e51420@ec2-3-211-228-251.compute-1.amazonaws.com:5432/d2md93msn8sj3j"
+SQLALCHEMY_DATABASE_URL = "your_database_uri"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL , use_native_hstore = False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
